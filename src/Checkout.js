aae9e313b79130d48b8e398cc3cceeb6a7710686
Fikrifire19 <muhamadfikri64@yahoo.co.id>
2020-10-30 22:45:20 +0700

    Membuat Username di Checkout Page dan Deploy ke Firebase

--- src/Checkout.js
+++ src/Checkout.js
@@ -8,13 +8,14 @@ import Subtotal from './Subtotal';
 
 
 function Checkout() {
-    const [{cart}, dispatch] = useStateValue();
+    const [{cart, user}, dispatch] = useStateValue();
 
     return (
         <div className="checkout">
             <div className="checkout-left">
                 <img className="checkout-ad" src="https://images-na.ssl-images-amazon.com/images/G/01/amazonglobal/images/PayCode/FinalAssets/Desktop/English/AmazonExports_FIAT_OnSite_Concepts_R3_DesktopStripe_1500x120_v1.png" alt="bannerAds" />
                 <div>
+                    <h4>Hello, {user?.email}</h4>
                     <h2 className="checkout-title">
                         Your Shopping Cart
                     </h2>
